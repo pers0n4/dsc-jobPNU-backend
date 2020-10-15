8a3ed1bb2fa842abf43864029ccaa33b6bb39cbf
Dong-Young Kim <31337.persona@gmail.com>
2020-10-15 15:46:01 +0900

    feat(app): add mongoose connection

--- app.js
+++ app.js
@@ -1,12 +1,30 @@
-const express = require("express");
+require("dotenv").config();
 const path = require("path");
+
+const express = require("express");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const mongoose = require("mongoose");
 
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
 
 const app = express();
+
+const connect = () => {
+  mongoose.connection
+    .on("error", () => console.error("error"))
+    .on("disconnected", connect)
+    .once("open", () => console.log("success"));
+  return mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  });
+};
+
+connect();
 
 app.use(logger("dev"));
 app.use(express.json());
