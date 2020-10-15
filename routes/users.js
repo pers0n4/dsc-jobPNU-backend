a3ac78daef83cb37a8cd4aa12ec61f5d8c4603b5
Dong-Young Kim <31337.persona@gmail.com>
2020-10-15 18:26:45 +0900

    feat(user): add user CRUD routes

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,59 @@
 const express = require("express");
 const router = express.Router();
 
-/* GET users listing. */
-router.get("/", function (req, res, next) {
-  res.send("respond with a resource");
+const User = require("../models/user");
+
+router.post("/", (req, res) => {
+  User.create(req.body)
+    .then((user) => {
+      res.status(201).json(user);
+    })
+    .catch((error) => {
+      res.status(400).send(error.message);
+    });
+});
+
+router.get("/", async (req, res) => {
+  const users = await User.find();
+  res.status(200).json(users);
+});
+
+router.get("/:_id", (req, res) => {
+  User.findById(req.params._id)
+    .then((user) => {
+      res.status(200).json(user);
+    })
+    .catch((error) => {
+      res.status(404).send(error.message);
+    });
+});
+
+router.patch("/", (req, res) => {
+  res.sendStatus(405);
+});
+
+router.patch("/:_id", (req, res) => {
+  User.findByIdAndUpdate(req.params._id, req.body, { new: true })
+    .then((user) => {
+      res.status(200).json(user);
+    })
+    .catch((error) => {
+      res.status(404).send(error.message);
+    });
+});
+
+router.delete("/", (req, res) => {
+  res.sendStatus(405);
+});
+
+router.delete("/:_id", (req, res) => {
+  User.findByIdAndDelete(req.params._id)
+    .then(() => {
+      res.sendStatus(200);
+    })
+    .catch((error) => {
+      res.status(404).send(error.message);
+    });
 });
 
 module.exports = router;
