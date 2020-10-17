195e955e6abf92387aebfb25cf88fd500f140821
Dong-Young Kim <31337.persona@gmail.com>
2020-10-17 18:05:44 +0900

    fix: remove public scaffolding

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,8 @@ const express = require("express");
 const router = express.Router();
 
 /* GET home page. */
-router.get("/", function (req, res, next) {
-  res.render("index", { title: "Express" });
+router.get("/", (req, res) => {
+  res.status(200).send("dsc-jobPNU Express API server");
 });
 
 module.exports = router;
