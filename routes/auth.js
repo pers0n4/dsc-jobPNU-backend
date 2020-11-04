a5cd0dd2e7ae443d5811a56a29ab280176968042
Dong-Young Kim <31337.persona@gmail.com>
2020-11-04 19:56:30 +0900

    feat(auth): add routes

--- routes/auth.js
+++ routes/auth.js
@@ -1,19 +1,40 @@
 const express = require("express");
+const passport = require("passport");
+const jwt = require("jsonwebtoken");
+require("../config/env");
+
 const router = express.Router();
 
-const User = require("../models/user");
+router.post("/", async (req, res, next) => {
+  passport.authenticate("local", async (error, user, info) => {
+    try {
+      if (error || !user) {
+        return res.sendStatus(401);
+      }
 
-router.post("/", async (req, res) => {
-  try {
-    const user = await User.findOne({ email: req.body.email }).exec();
-    if (await user.verifyPassword(req.body.password)) {
-      res.status(200).send("success");
-    } else {
-      throw Error();
+      req.login(user, { session: false }, async (error) => {
+        if (error) {
+          return next(error);
+        }
+
+        const token = jwt.sign(user.toJSON(), process.env.JWT_SECRET, {
+          expiresIn: "1h",
+        });
+
+        res.status(200).json({ token });
+      });
+    } catch (error) {
+      next(error);
     }
-  } catch (error) {
-    res.status(401).send("fail");
-  }
+  })(req, res, next);
 });
+
+router.get(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    res.status(200).json(req.user);
+  }
+);
 
 module.exports = router;
