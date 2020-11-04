39015fe0c48f614f1110726a925996e615c1012e
Dong-Young Kim <31337.persona@gmail.com>
2020-11-05 01:59:50 +0900

    refactor(auth): change import order

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,9 @@
 const express = require("express");
+const router = express.Router();
+
 const passport = require("passport");
 const jwt = require("jsonwebtoken");
 require("../config/env");
-
-const router = express.Router();
 
 /**
  * @openapi
