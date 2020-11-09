538d96ebd0c5629a12ea2f84634c0daee523e9e6
Dong-Young Kim <31337.persona@gmail.com>
2020-11-09 17:54:41 +0000

    style: format code

--- models/study.js
+++ models/study.js
@@ -1,16 +1,17 @@
 // models/study.js
 
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // schema
-const studySchema = mongoose.Schema({ // 1
-  title:{type:String, required:true},
-  field:{type:String, required:true},
-  num:{type:Number,required:true},
-  time:{type:String,required:true},
-  period:{type:String,required:true},
-  createdAt:{type:Date, default:Date.now}, // 2
-  updatedAt:{type:Date},
+const studySchema = mongoose.Schema({
+  // 1
+  title: { type: String, required: true },
+  field: { type: String, required: true },
+  num: { type: Number, required: true },
+  time: { type: String, required: true },
+  period: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now }, // 2
+  updatedAt: { type: Date },
 });
 
 // model & export
