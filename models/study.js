017bda276c0f71ac7fabe77ad242f0230fcb3b5b
miny4971 <miny4971@gmail.com>
2020-11-25 23:27:50 +0900

    스터디 대기,승인,거절상태 기능 추가

--- models/study.js
+++ models/study.js
@@ -1,41 +1,42 @@
 // models/study.js
 
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const pointSchema = mongoose.Schema({
   type: {
     type: String,
-    enum: ['Point'],
-    required: true
+    enum: ["Point"],
+    required: true,
   },
   coordinates: {
     type: [Number],
-    required: true
-  }
+    required: true,
+  },
 });
 //member
 const memberSchema = mongoose.Schema({
-  _member : {type: mongoose.Schema.Types.ObjectId, ref: 'User', index: true}
+  member: { type: mongoose.Schema.Types.ObjectId, ref: "User", index: true, unique : true },
+  status: { type : String }
 });
-
 
 // schema
-const studySchema = mongoose.Schema({ // 1
-  user: {type: mongoose.Schema.Types.ObjectId, ref: 'User', index: true },
-  title:{type:String, required:true},
-  field:{type:String, required:true},
-  location:{type: pointSchema,required:true},
-  num:{type:Number,required:true},
-  content:{type:String},
-  start_date:{type:Date,required:true},
-  end_date:{type:Date,required:true},
-  createdAt:{type:Date, default:Date.now}, // 2
-  updatedAt:{type:Date},
-  members : {type:[memberSchema]}
+const studySchema = mongoose.Schema({
+  // 1
+  user: { type: mongoose.Schema.Types.ObjectId, ref: "User", index: true },
+  title: { type: String, required: true },
+  field: { type: String, required: true },
+  location: { type: pointSchema, required: true },
+  num: { type: Number, required: true },
+  content: { type: String },
+  start_date: { type: Date, required: true },
+  end_date: { type: Date, required: true },
+  createdAt: { type: Date, default: Date.now }, // 2
+  updatedAt: { type: Date },
+  members: { type: [memberSchema] },
 });
 
-studySchema.methods.mem = function (member) {
-  this.members.push({ _member: member});
+studySchema.methods.mem = function (member, status) {
+  this.members.push({ member: member , status});
   return this.save();
 };
 
