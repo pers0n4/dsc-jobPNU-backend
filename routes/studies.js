11b813188c371d4004111a367e76a6872c1e0a6f
miny4971 <miny4971@gmail.com>
2020-12-27 21:41:52 +0900

    수정 및 삭제

--- routes/studies.js
+++ routes/studies.js
@@ -4,6 +4,13 @@ const express = require("express");
 const router = express.Router();
 const Study = require("../models/study");
 
+const verifyStudyIdentity = (req, res, next) => {
+  if (req.study.id !== req.params.id) {
+    return res.sendStatus(401);
+  }
+  next();
+};
+
 /**
  * @openapi
  * components:
@@ -109,6 +116,18 @@ router.get("/", async (req, res) => {
   res.status(200).json(studies);
 });
 
+router.put("/", (req, res) => {
+  res.sendStatus(405);
+});
+
+router.patch("/", (req, res) => {
+  res.sendStatus(405);
+});
+
+router.delete("/", (req, res) => {
+  res.sendStatus(405);
+});
+
 /**
  * @openapi
  * /studies/{id}:
@@ -139,6 +158,39 @@ router.get("/:id", function (req, res) {
     res.status(200).json(study);
   });
 });
+
+router.put("/:id", (req, res) => {
+  res.sendStatus(405);
+});
+
+router.patch("/:id", verifyStudyIdentity, (req, res) => {
+  Study.findByIdAndUpdate(req.params.id, req.body, { new: true })
+    .then((study) => {
+      res.status(200).json(study);
+    })
+    .catch((error) => {
+      res.status(404).send(error.message);
+    });
+});
+
+router.delete(
+  "/:id",
+  (req, res) => {
+    Study.findByIdAndDelete(req.params.id)
+      .then(() => {
+        res.sendStatus(204);
+      })
+      .catch((error) => {
+        res.status(404).send(error.message);
+      });
+  }
+);
+
+// router.delete("/:id", function (req, res) {
+//   Study.findById(req.params.id).then((study) => {
+//     res.status(200).json(study);
+//   });
+// });
 
 /**
  * @openapi
